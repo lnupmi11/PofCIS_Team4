e3f744b221fb7837ed547f1a8373ab9da4586280
olegdatskiv <olegdatskiv12@gmail.com>
2018-09-18 18:19:19 +0300

    added class,interface and override ToString

---
+++ Currency.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Task4Currency
+{
+    
+    public class Currency : IReadable
+    {
+        public string CurrencyName { get; set; }
+
+        public double Amount { get; set; }
+
+        public void Read(StreamReader sr)
+        {
+            var inputData = sr.ReadLine().Split();
+            int amount, temp;
+
+            if (Int32.TryParse(inputData[0], out amount) && !Int32.TryParse(inputData[1],out temp))
+            {
+                CurrencyName = inputData[1];
+
+                Amount = amount;
+            }
+
+        }
+
+        public override string ToString()
+        {
+            return $"{Amount} {CurrencyName}";
+        }
+
+    }
+}
---
+++ IReadable.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+
+namespace Task4Currency
+{
+    public interface IReadable
+    {
+        void Read(StreamReader sr);
+    }
+}
