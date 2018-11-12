8460e81490a90fccb774759ebbe31a270233e9d6
DRomaniv <d.romanivvv@gmail.com>
2018-11-12 23:23:25 +0200

    Added User Interface to BusinessLogic

---
+++ DoctorsAppointments/ViewModel/DoctorWithProfitViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace DoctorsAppointments.ViewModel
+{
+    public class DoctorWithProfitViewModel
+    {
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string SecondName { get; set; }
+
+        public double PriceForConsultation { get; set; }
+
+        public double Profit { get; set; }
+    }
+}
