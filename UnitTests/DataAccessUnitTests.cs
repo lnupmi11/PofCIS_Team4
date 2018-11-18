819a18d6c4e55296e19e93202ee72335e680561c
Nazar <nazar.hrytsiv7@gmail.com>
2018-11-19 00:13:32 +0300

    adding tests

---
+++ UnitTests/DataAccessUnitTests.cs
@@ -0,0 +1,114 @@
+﻿using BusinessLogic;
+using DataAccess;
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace UnitTests
+{
+    [TestFixture]
+    public class DataAccessUnitTests
+    {
+        private string directory = "D:\\";
+        private string filename = "doctors.xml";
+        private UnitOfWork unitOfWork;
+        private XmlSerializer serializer;
+
+        [SetUp]
+        public void SetUp()
+        {
+            serializer = new XmlSerializer(typeof(List<Doctor>));
+            unitOfWork = new UnitOfWork(directory);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            var doctors = unitOfWork.Doctors.ReadAll();
+            for(int i = 0; i < doctors.Count; i++)
+            {
+                unitOfWork.Doctors.Delete(doctors[i].Id);
+            }
+        }
+
+        [Test]
+        public void RepositoryCreate_ValidData_WritesToFile()
+        {
+            var doc1 = new Doctor
+            {
+                Id = 1,
+                PriceForConsultation = 100.5,
+                FirstName = "Doctor",
+                SecondName = "Watson",
+                Specialization = Specialization.Oculist
+            };
+
+            unitOfWork.Doctors.Create(doc1);
+            var res = unitOfWork.Doctors.Read(1);
+
+            Assert.AreEqual(res.FirstName, "Doctor");
+            Assert.AreEqual(res.SecondName, "Watson");
+            Assert.AreEqual(res.Id, 1);
+            Assert.AreEqual(res.PriceForConsultation, 100.5);
+            Assert.AreEqual(res.Specialization, Specialization.Oculist);
+        }
+
+        [Test]
+        public void RepositoryRead_IdDoesntExist_ReturnsNull()
+        {
+            Assert.IsNull(unitOfWork.Doctors.Read(2000));
+        }
+
+        [Test]
+        public void RepositoryDelete_ValidInput_DoctorDeleted()
+        {
+            var doc1 = new Doctor
+            {
+                Id = 1,
+                PriceForConsultation = 100.5,
+                FirstName = "Doctor",
+                SecondName = "Watson",
+                Specialization = Specialization.Oculist
+            };
+
+            unitOfWork.Doctors.Create(doc1);
+            unitOfWork.Doctors.Delete(2000);
+            Assert.IsNull(unitOfWork.Doctors.Read(2000));
+        }
+
+        [Test]
+        public void RepositoryUpdate_DoctorWithNewPrice_PriceOfDoctorUpdated()
+        {
+            var doc1 = new Doctor
+            {
+                Id = 1,
+                PriceForConsultation = 100.5,
+                FirstName = "Doctor",
+                SecondName = "Watson",
+                Specialization = Specialization.Oculist
+            };
+
+            var doc1Updated = new Doctor
+            {
+                Id = 1,
+                PriceForConsultation = 200.5,
+                FirstName = "Doctor",
+                SecondName = "Watson",
+                Specialization = Specialization.Oculist
+            };
+
+
+
+            unitOfWork.Doctors.Create(doc1);
+            unitOfWork.Doctors.Update(doc1Updated.Id, doc1Updated);
+
+            var res = unitOfWork.Doctors.Read(1);
+            Assert.AreEqual(res.PriceForConsultation, 200.5);
+            Assert.AreEqual(res.FirstName, "Doctor");
+            Assert.AreEqual(res.SecondName, "Watson");
+            Assert.AreEqual(res.Id, 1);
+            Assert.AreEqual(res.Specialization, Specialization.Oculist);
+        }
+    }
+}
---
+++ UnitTests/FinancialOperationsUnitTests.cs
@@ -0,0 +1,183 @@
+﻿using BusinessLogic;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTests
+{
+    [TestFixture]
+    public class FinancialOperationsUnitTests
+    {
+        [Test]
+        public void GetProfitFromSellingMedicament_IdDoesntExist_ThrowsNullReferenceException()
+        {
+            var medicaments = new List<MedicalEquipment>
+            {
+                new MedicalEquipment
+                {
+                    Id= 1
+                },
+                new MedicalEquipment
+                {
+                    Id= 2
+                }
+            };
+            Assert.Throws<NullReferenceException>(() => FinancialOperations.GetProfitFromSellingMedicament(1000, medicaments));
+        }
+
+        [Test]
+        public void GetProfitFromSellingMedicament_ListIsNull_ThrowsArgumentNullException()
+        {
+            Assert.Throws<ArgumentNullException>(() => FinancialOperations.GetProfitFromSellingMedicament(1000, null));
+        }
+
+        [Test]
+        public void GetProfitFromSellingMedicament_ValidInput_ReturnsValidProfit()
+        {
+            var medicaments = new List<MedicalEquipment>
+            {
+                new MedicalEquipment
+                {
+                    Id= 1,
+                    Code = "ab45s",
+                    Price = 10.0,
+                    AmountBought = 100,
+                    AmountAvailable = 5
+                }
+            };
+            Assert.AreEqual(FinancialOperations.GetProfitFromSellingMedicament(1, medicaments), 1000.0);
+        }
+
+        [Test]
+        public void GetProfitFromDoctorConsultations_IdDoesntExist_ThrowsNullReferenceException()
+        {
+            var doctors = new List<Doctor>
+            {
+                new Doctor
+                {
+                    Id= 1
+                }
+            };
+
+            var apps = new List<Appointment>
+            {
+                new Appointment
+                {
+                    Id= 1
+                }
+            };
+
+
+            Assert.Throws<NullReferenceException>(() => FinancialOperations.GetProfitFromDoctorConsultations(1000, apps, doctors));
+        }
+
+        [Test]
+        public void GetProfitFromDoctorConsultations_ListsAreNull_ThrowsArgumentNullException()
+        { 
+            Assert.Throws<ArgumentNullException>(() => FinancialOperations.GetProfitFromDoctorConsultations(1000, null, null));
+        }
+
+        [Test]
+        public void GetProfitFromDoctorConsultations_ValidInput_ReturnsValidProfit()
+        {
+            var doctors = new List<Doctor>
+            {
+                new Doctor
+                {
+                    Id= 1,
+                    PriceForConsultation = 10.0
+                }
+            };
+
+            var apps = new List<Appointment>
+            {
+                new Appointment
+                {
+                    Id= 1,
+                    DoctorId = 1,
+                    PatientId = 1
+                },
+                new Appointment
+                {
+                    Id= 2,
+                    DoctorId = 1,
+                    PatientId = 2
+                },
+                new Appointment
+                {
+                    Id= 3,
+                    DoctorId = 1,
+                    PatientId = 3
+                }
+            };
+            Assert.AreEqual(FinancialOperations.GetProfitFromDoctorConsultations(1, apps, doctors), 30.0);
+        }
+
+        [Test]
+        public void GetMostProductiveDoctor_ValidInput_ReturnsValidMostProductiveDoctor()
+        {
+            var doctors = new List<Doctor>
+            {
+                new Doctor
+                {
+                    Id= 1,
+                    PriceForConsultation = 10.0,
+                    FirstName = "Mary",
+                    SecondName = "Novosad"
+                },
+                new Doctor
+                {
+                    Id= 2,
+                    PriceForConsultation = 15.0,
+                    FirstName = "Ivan",
+                    SecondName = "Ivanovich"
+                }
+            };
+
+            var apps = new List<Appointment>
+            {
+                new Appointment
+                {
+                    Id= 1,
+                    DoctorId = 1,
+                    PatientId = 1
+                },
+                new Appointment
+                {
+                    Id= 2,
+                    DoctorId = 1,
+                    PatientId = 2
+                },
+                new Appointment
+                {
+                    Id= 3,
+                    DoctorId = 1,
+                    PatientId = 3
+                },
+
+                new Appointment
+                {
+                    Id= 4,
+                    DoctorId = 2,
+                    PatientId = 1
+                },
+                new Appointment
+                {
+                    Id= 5,
+                    DoctorId = 2,
+                    PatientId = 2
+                },
+                new Appointment
+                {
+                    Id= 6,
+                    DoctorId = 2,
+                    PatientId = 3
+                }
+            };
+            Assert.AreEqual(FinancialOperations.GetMostProductiveDoctor(doctors, apps), "Ivan Ivanovich");
+        }
+    }
+}
